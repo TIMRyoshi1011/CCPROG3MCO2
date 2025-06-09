630c264312c9110982ec867a81e7b3b2a28fe7ac
coby <jycluna2808@gmail.com>
2025-06-09 20:37:46 +0800

    Added placeholder files
    All empty (except for Location) files. All the classes  we'll have according to the current UML. Will add empty methods and attributes according to the UML in a bit.

---
+++ Location.java
@@ -0,0 +1,26 @@
+public class Location{
+	private final String NAME;
+	private final String FAVORED_DRINK;
+	private final String DISLIKED_DRINK;
+	private int truck = -1;
+
+	public Location(String name, String favorite, String dislike){
+		// blah
+	}
+
+	public boolean setTruck(int indx){
+		return false;
+	}
+
+	public void removeTruck(){
+		// blah
+	}
+
+	public String getFavorite(){
+		return "blah";
+	}
+
+	public String getDislike(){
+		return "blah";
+	}
+}
