d7d4f0afb5b63d5ea902bf6f91068b87d479d529
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-06-12 17:42:48 +0800

    Update CoffeeTruck.java

--- CoffeeTruck.java
+++ CoffeeTruck.java
@@ -8,18 +8,31 @@ public class CoffeeTruck(){
 	private final ArrayList<StorageBin> STORAGEBINS;
 
 	public CoffeeTruck(String location){
-
+		this.truckLocation = location.toString();
+		this.moneyEarned = 0;
 	}
 
 	public boolean fillStorageBin(){
+		if(STORAGEBINS.isEmpty()) // not sure about this
+			return true;
 
+		else
+			return false;
 	}
 
 	public void simulateSale(){
-
+		/*
+		Example:
+		>>> Preparing Medium Cup...
+		>>> Brewing Standard espresso - 1.49 grams of coffee...
+		>>> Adding Milk...
+		>>> Cappuccino Done!
+		 */
 	}
 
 	public void printTruckInfo(){
-		
+		System.out.println("Truck Type: " + truckType);
+	        System.out.println("Truck Location: " + truckLocation);
+	        System.out.println("Money Earned: $" + moneyEarned);
 	}
 }
