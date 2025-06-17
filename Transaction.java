a6a8dc2a3eb26e8c49805f8d6e91329d3c9b7092
coby <jycluna2808@gmail.com>
2025-06-17 06:28:14 +0000

    Update Transaction.java
    Added changes in UML and added complete javadoc for all attributes and methods.

--- Transaction.java
+++ Transaction.java
@@ -1,15 +1,42 @@
 import java.util.ArrayList;
 
+/**
+ * Represents a transaction.
+ * A list of all transactions is recorded in all coffee trucks.
+ * Records the type, ingredients used, and cost of a transation.
+ * @author Coby Luna & Marcus Ramos
+ */
 public class Transaction{
+	/** Character representation of drink size. */
 	private char drinkSize;
+	/** String representation of drink type. */
 	private String drinkType;
+	/** The total cost of the drink. */
 	private float drinkCost;
-	private final ArrayList<Ingredient> INGREDIENTS;
+	/** ArrayList of all ingredients used in the transaction. */
+	private ArrayList<Ingredient> ingredients;
 
-	public Trandsactions(String type, char Size){
+	/**
+ 	 * Construction for transaction.
+   	 * Upon construction all transaction details are calculated, including ingredient amount and cost.
+     	 * @param type The drink type
+       	 * @param size The size of the drink. 
+	 */
+	public Transactions(String type, char size){
 
 	}
 
+	/**
+ 	 * Returns the price of the transation.
+   	 * @return Price of the transaction
+     	 */
+	public float getPrice(){
+		return 0;
+	}
+
+	/**
+ 	 * Prints all transaction info.
+   	 */
 	public void printTransaction(){
 		
 	}
