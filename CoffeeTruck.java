528fa5dad6207ffe290364e8753c8ab9effc9575
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-06-17 17:20:55 +0800

    Update CoffeeTruck.java

--- CoffeeTruck.java
+++ CoffeeTruck.java
@@ -10,29 +10,43 @@ public class CoffeeTruck(){
 	public CoffeeTruck(String location){
 		this.truckLocation = location.toString();
 		this.moneyEarned = 0;
+		this.STORAGEBINS = new ArrayList<>();
+	        this.TRANSACTIONS = new ArrayList<>();
+	
+	        for (int i = 0; i < 3; i++) {
+	            STORAGEBINS.add(new StorageBin());
+	        }
 	}
 
-	public boolean fillStorageBin(){
-		if(STORAGEBINS.isEmpty()) // not sure about this
-			return true;
-
-		else
-			return false;
+	public boolean fillStorageBin(int storageBinIndx){
+		if (index >= 0 && index < STORAGEBINS.size()) {
+	            return STORAGEBINS.get(storageBinIndx).setBin("coffee", 100);
+	        }
+	
+	        return false;
 	}
 
-	public void simulateSale(){
-		/*
-		Example:
-		>>> Preparing Medium Cup...
-		>>> Brewing Standard espresso - 1.49 grams of coffee...
-		>>> Adding Milk...
-		>>> Cappuccino Done!
-		 */
-	}
+	public boolean setType(char type) {
+		TRUCK_TYPE = type;
+		return true;
+    	}
 
-	public void printTruckInfo(){
-		System.out.println("Truck Type: " + truckType);
-	        System.out.println("Truck Location: " + truckLocation);
-	        System.out.println("Money Earned: $" + moneyEarned);
+	public boolean setLocation(String location) {
+		this.truckLocation = location;
+		return true;
+	}
+	
+	public ArrayList<Character> returnMenu() {
+		return menu;
+	}
+	
+	public void simulateSale() {
+		Transaction t = new Transaction("Latte", 'M');
+		moneyEarned += t.getPrice();
+		TRANSACTIONS.add(t);
+	}
+	
+	public void printTruckInfo() {
+		System.out.println("Truck at: " + truckLocation + " | Earned: " + moneyEarned);
 	}
 }
