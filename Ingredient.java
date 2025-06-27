cd33f653e057fe4cc9fdd9647f766e5ea8b411e5
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-06-28 00:53:14 +0800

    Update Ingredient.java

--- Ingredient.java
+++ Ingredient.java
@@ -23,7 +23,7 @@ public class Ingredient{
 	/** Static variable for the price of 1 gram of coffeebeans. */
 	private static float coffeePrice = 1;
 	/** Static variable for the price of 1 fl of milk. */
-	private static float milkPrice = 2f;						// added f at the end of the values (causes error if there is no f)
+	private static float milkPrice = 2f;
 	/** Static variable for the price of 1 fl of water. */
 	private static float waterPrice = 0.5f;
 
@@ -116,3 +116,4 @@ public class Ingredient{
 	        }
 	}
 }
+
