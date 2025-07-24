1c39558b75be66dddfc03aaba68e9d1fef0faae4
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-07-25 02:35:28 +0800

    GUI Files are here

--- App/SettingStorageBins.java
+++ App/SettingStorageBins.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class SettingStorageBins extends JFrame implements ActionListener {
+	private AppModel model;
 
 	private StorageBin storageBin;
 	private SetBinAmount setAmount;
